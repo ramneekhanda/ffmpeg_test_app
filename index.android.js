270303b3658ce685db61daedaee704baea229aa6
Ramneek Handa <RamneekHanda@gmail.com>
2017-06-14 17:39:48 +0800

    Initial checkin

---
+++ index.android.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import RNFFMpeg from 'rn-ffmpeg';
+
+export default class ffmpeg_test_app extends Component {
+
+  _onPressButton() {
+    RNFFMpeg.encodeVideo(null, null)
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome} onPress={this._onPressButton}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ffmpeg_test_app', () => ffmpeg_test_app);
